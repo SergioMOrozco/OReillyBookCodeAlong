603c0430a2b2b2b117fb19177d19799860b5045e
Sergio Orozco <sorozco0612@yahoo.com>
2020-01-30 19:57:30 -0700

    Began creation of Softmax Regression

--- Chapter 4 - Training/chapter_4_exercise_12.py
+++ Chapter 4 - Training/chapter_4_exercise_12.py
@@ -1,3 +1,14 @@
 ## Exercise 12 : 
 # Implement Batch Gradient Descent with early stopping
 # for Softmax Regression (without using Scitkit-Learn).
+
+from sklearn import datasets
+import soft_max_scratch
+import numpy as np
+iris = datasets.load_iris()
+
+soft_clf = soft_max_scratch.SoftMaxClassifier(np.random.rand(1,2), [0,1,2])
+print(soft_clf.calculate_softmax(0))
+
+
+
---
+++ Chapter 4 - Training/soft_max_scratch.py
@@ -0,0 +1,29 @@
+import numpy as np
+import math
+
+class SoftMaxClassifier ():
+
+    def __init__(self,X,y):
+        self._x = X
+        self._y = y 
+        self._class_count = len(np.unique(y))
+        self._weights = np.random.rand(self._class_count,X.shape[1])
+
+    def calculate_score(self,class_index):
+        weight = self._weights[class_index]
+        return self._x.dot(weight)
+
+    def calculate_softmax(self,class_index):
+        sum_of_exp = 0
+        for i in range (self._class_count):
+            sum_of_exp += self.calculate_score(i)
+
+        return self.calculate_score(class_index) / sum_of_exp
+
+    def calculate_cross_entropy(self,class_index):
+        
+
+
+        
+
+
