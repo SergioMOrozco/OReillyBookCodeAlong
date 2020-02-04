0dda82296cf8691bad182d60c196525f2bd23282
Sergio Orozco <sorozco0612@yahoo.com>
2020-02-03 20:20:58 -0700

    created cross entropy gradient function

--- Chapter 4 - Training/chapter_4_exercise_12.py
+++ Chapter 4 - Training/chapter_4_exercise_12.py
@@ -7,8 +7,9 @@ import soft_max_scratch
 import numpy as np
 iris = datasets.load_iris()
 
-soft_clf = soft_max_scratch.SoftMaxClassifier(np.random.rand(1,2), [0,1,2])
-print(soft_clf.calculate_softmax(0))
+x = np.random.rand(1,2)
+soft_clf = soft_max_scratch.SoftMaxClassifier(x, [[1,0,0]])
+print(soft_clf.calculate_cross_entropy_gradient(0)) 
 
 
 
--- Chapter 4 - Training/soft_max_scratch.py
+++ Chapter 4 - Training/soft_max_scratch.py
@@ -6,22 +6,27 @@ class SoftMaxClassifier ():
     def __init__(self,X,y):
         self._x = X
         self._y = y 
-        self._class_count = len(np.unique(y))
+        self._class_count = len(self._y[0])
         self._weights = np.random.rand(self._class_count,X.shape[1])
 
-    def calculate_score(self,class_index):
+    def calculate_score(self,class_index,instance_index):
         weight = self._weights[class_index]
-        return self._x.dot(weight)
+        return self._x[instance_index].dot(weight)
 
-    def calculate_softmax(self,class_index):
+    def calculate_softmax(self,class_index,instance_index):
         sum_of_exp = 0
         for i in range (self._class_count):
-            sum_of_exp += self.calculate_score(i)
+            sum_of_exp += self.calculate_score(i,instance_index)
 
-        return self.calculate_score(class_index) / sum_of_exp
+        return self.calculate_score(class_index,instance_index) / sum_of_exp
 
-    def calculate_cross_entropy(self,class_index):
-        pass
+    def calculate_cross_entropy_gradient(self,class_index):
+        sum = 0
+        for i in range (len(self._x)):
+            sum += ((self.calculate_softmax(class_index,i) - self._y[i][class_index]) * self._x[i])
+
+        return sum / len(self._x)
+
 
 
         
