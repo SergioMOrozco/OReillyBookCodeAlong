1aa902a8b686578ff70dcef33d83a4f4f9ca95a2
FezTheImmigrant <39529018+FezTheImmigrant@users.noreply.github.com>
2020-02-08 23:52:46 -0700

    Added todo

--- Chapter 4 - Training/chapter_4_exercise_12.py
+++ Chapter 4 - Training/chapter_4_exercise_12.py
@@ -5,12 +5,15 @@
 from sklearn import datasets
 import soft_max_scratch
 import numpy as np
-iris = datasets.load_iris()
 
+
+iris = datasets.load_iris()
 data = iris["data"]
 labels = iris["target"]
 
+## Converts class labels to one hot encodings
 def convert_to_one_hot(labels):
+
     class_count = len(set(labels)) 
     one_hot = np.zeros((len(labels),class_count))
     
@@ -18,14 +21,19 @@ def convert_to_one_hot(labels):
         one_hot[i][labels[i]] = 1
 
     return one_hot
-labels_one_hot = convert_to_one_hot(labels)
-rand = np.random.permutation(len(data))
 
+labels_one_hot = convert_to_one_hot(labels)
+
+## shuffle training data and labels (Gradient Descent performs better this way)
+rand = np.random.permutation(len(data))
 x,y = data[rand] , labels_one_hot[rand]
 
-soft_clf = soft_max_scratch.SoftMaxClassifier(learning_rate = 0.5, max_iter = 1000, minimum_step_size = 0, early_stopping = True)
+## Create and train SoftmaxClassifier on training data
+soft_clf = soft_max_scratch.SoftMaxClassifier(learning_rate = 0.5, max_iter = 10000, minimum_step_size = 0.001, early_stopping = True)
 soft_clf.train(x,y)
 
+
+##TODO: Predict on test data. not testing data
 y_pred = soft_clf.predict(x)
 
 count = 0
